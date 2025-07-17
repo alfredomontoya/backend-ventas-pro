9525a18a5e03d332ce6730d6b3266b3dc59f8c7c
alfredomontoya <39633080+alfredomontoya@users.noreply.github.com>
2025-07-16 21:42:03 -0400

    correccion controlador Truncate tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,7 +36,7 @@ class DatabaseSeeder extends Seeder
             'derivacions'
         ]);
 
-        
+
         $this->call([
             UserSeeder::class,
             ProductoSeeder::class,
@@ -49,31 +49,34 @@ class DatabaseSeeder extends Seeder
     }
 
     protected function truncateTables(array $tables)
-    {
-        $connection = DB::getDriverName();
+{
+    $connection = DB::getDriverName();
 
+    if ($connection === 'mysql') {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+    } elseif ($connection === 'sqlite') {
+        DB::statement('PRAGMA foreign_keys = OFF;');
+    }
+
+    foreach ($tables as $table) {
+        // Eliminar datos
+        DB::table($table)->delete();
+
+        // Reset autoincrement según el motor
         if ($connection === 'mysql') {
-            DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
-        } elseif ($connection === 'sqlite') {
-            DB::statement('PRAGMA foreign_keys = OFF;');
-        }
-
-        foreach ($tables as $table) {
-            // Usa delete() para mayor compatibilidad con SQLite
-            DB::table($table)->delete();
             DB::statement("ALTER TABLE `$table` AUTO_INCREMENT = 1");
-            // También puedes resetear los IDs si quieres en SQLite
-            if ($connection === 'sqlite') {
-                DB::statement("DELETE FROM sqlite_sequence WHERE name = '$table'");
-            }
-        }
-
-        if ($connection === 'mysql') {
-            DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
         } elseif ($connection === 'sqlite') {
-            DB::statement('PRAGMA foreign_keys = ON;');
+            DB::statement("DELETE FROM sqlite_sequence WHERE name = '$table'");
         }
     }
 
-    
+    if ($connection === 'mysql') {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
+    } elseif ($connection === 'sqlite') {
+        DB::statement('PRAGMA foreign_keys = ON;');
+    }
+}
+
+
+
 }
