41271a4faaeaae3fc10a7dd561272322735e49ca
alfredomontoya <39633080+alfredomontoya@users.noreply.github.com>
2025-07-16 22:35:06 -0400

    desplegar react

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+Route::get('/{any}', function () {
+    $path = public_path('react/index.html');
+    if (!File::exists($path)) {
+        abort(404);
+    }
+    return Response::file($path);
+})->where('any', '.*');
